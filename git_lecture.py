430b8ddcf0d63f1d59c04941c0c052acb44d0ca9
Sergey Morya <sergey.morya@gmail.com>
2019-03-21 18:42:03 +0200

    second changes

--- git_lecture.py
+++ git_lecture.py
@@ -1,6 +1,10 @@
 SSN_white_list = [1324,5678,9101]
 while True:
     user_ssn_number = input('Enter your SSN number in format (0000): ')
+    #length checker
     if len(user_ssn_number) != 4:
         print('Invalid SSN. Please check the format (0000)!!!')
+        continue
+    if not user_ssn_number.isdigit():
+        print("Please enter digit only")
         continue
