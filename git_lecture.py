c8736e3fe095048daa65b552f807e4a248518d70
Sergey Morya <sergey.morya@gmail.com>
2019-03-22 12:02:08 +0200

    feature_02 is ready

--- git_lecture.py
+++ git_lecture.py
@@ -5,9 +5,12 @@ while True:
     if len(user_ssn_number) != 4:
         print('Invalid SSN. Please check the format (0000)!!!')
         continue
+    #digit checker
     if not user_ssn_number.isdigit():
         print("Please enter digit only")
         continue
+    #white list checker
     if int(user_ssn_number) not in SSN_white_list:
         print("Your premissions are prohibitted, please contact with your manager")
         continue
+print("You are welcome")
