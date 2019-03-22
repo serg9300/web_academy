47320fd4785b023c53206ef965e3c8df268efc52
Sergey Morya <sergey.morya@gmail.com>
2019-03-22 11:56:30 +0200

    five number ssn added

--- git_lecture.py
+++ git_lecture.py
@@ -1,8 +1,8 @@
-SSN_white_list = [1324,5678,9101]
+SSN_white_list = [1324,5678,9101,44557]
 while True:
     user_ssn_number = input('Enter your SSN number in format (0000): ')
     #length checker
-    if len(user_ssn_number) != 4:
+    if len(user_ssn_number) not in (4,5):
         print('Invalid SSN. Please check the format (0000)!!!')
         continue
     if not user_ssn_number.isdigit():
