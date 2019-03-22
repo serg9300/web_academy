7f836fc83af35ae5a36ee84009391e64046705b1
Sergey Morya <sergey.morya@gmail.com>
2019-03-22 18:19:22 +0200

    add isinstance to arithmetic

--- fuction_arithmetic.py
+++ fuction_arithmetic.py
@@ -2,7 +2,8 @@ a = input('Enter first number: ')
 b = input('Enter second number: ')
 calculation = input('Enter argument (+, -, /, *): ')
 def arithmetic(a,b,calculation):
-
+    if not isinstance(a, (int,float)) or not isinstance(b, (int, float)):
+        return "Error"
     if calculation == '/':
         return int(a)/int(b)
     elif calculation == '*':
@@ -13,5 +14,9 @@ def arithmetic(a,b,calculation):
         return int(a) + int(b)
     else:
         return "Unknown operation"
-x=arithmetic(a,b,calculation)
-print(x)
+
+if __name__ == '__main__':
+    arithmetic(a,b,calculation)
+    arithmetic(a, b, calculation)
+    arithmetic(a, b, calculation)
+    arithmetic(a, b, calculation)
